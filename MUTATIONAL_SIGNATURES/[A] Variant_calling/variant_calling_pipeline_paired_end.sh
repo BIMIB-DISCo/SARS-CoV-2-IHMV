497031beb04825d1506f9d7bf49fd7d6265fe9ea
alexgraudenzi <alex.graudenzi@unimib.it>
2020-07-07 13:59:37 +0200

    Variant calling scripts

---
+++ MUTATIONAL_SIGNATURES/[A] Variant_calling/variant_calling_pipeline_paired_end.sh
@@ -0,0 +1,151 @@
+#
+
+### variant_calling_pipeline_paired_end.sh
+
+#  REQUIRED EXTERNAL TOOLS:
+#    trimmomatic
+#    bwa
+#    samtools
+#    picard
+#    varscan
+
+# SETTINGS
+
+# Directories.
+
+basedir="/analysis_directory/raw_data/"
+resultsdir="/analysis_directory/results/"
+fastqTrimDir=${resultsdir}trimmed/
+bwaSamDir=${resultsdir}bwasam/
+bamDir=${resultsdir}bamDir/
+vcfDir=${resultsdir}vcfDir/
+
+
+# Name of the paired-end sample to be analyzed.
+
+sampleName="ABCDE"
+
+
+# Reference genome.
+# Reference to be used in the analysis.
+
+genomeFa="analysis_directory/reference/reference.fasta"
+
+
+# Other setup.
+
+jobs=4
+
+
+#===B1===
+
+echo "trimmomatic -- trimming fastq"
+
+mkdir -p $fastqTrimDir
+if [ ! -d "$fastqTrimDir" ]; then
+    echo "Error mkdir $fastqTrimDir"
+    exit 1
+fi
+
+# We use custom parameters as described in Bastola, Anup, et al. "The
+# first 2019 novel coronavirus case in Nepal." The Lancet Infectious
+# Diseases 20.3 (2020): 279-280.
+
+# LEADING:20 TRAILING:20 SLIDINGWINDOW:4:20 MINLEN:40
+
+trimmomatic PE -threads $jobs -phred33 \
+	    -summary ${fastqTrimDir}${sampleName}_trim.summary \
+	    -quiet \
+	    -validatePairs \
+	    ${basedir}${sampleName}_1.fastq \
+	    ${basedir}${sampleName}_2.fastq \
+	    ${fastqTrimDir}${sampleName}_1_paired.trim.fastq.gz \
+	    ${fastqTrimDir}${sampleName}_1_unpaired.trim.fastq.gz \
+	    ${fastqTrimDir}${sampleName}_2_paired.trim.fastq.gz \
+	    ${fastqTrimDir}${sampleName}_2_unpaired.trim.fastq.gz \
+	    LEADING:20 \
+	    TRAILING:20 \
+	    SLIDINGWINDOW:4:20 \
+	    MINLEN:40
+
+#===E1===
+
+#===B2===
+
+echo "bwa mem -- mapping reads to a reference SARS-CoV-2"
+
+mkdir -p $bwaSamDir
+if [ ! -d "$bwaSamDir" ]; then
+    echo "Error mkdir $bwaSamDir"
+    exit 1
+fi
+
+bwa mem -t $jobs $genomeFa \
+    ${fastqTrimDir}${sampleName}_1_paired.trim.fastq.gz \
+    ${fastqTrimDir}${sampleName}_2_paired.trim.fastq.gz \
+    > ${bwaSamDir}${sampleName}_aln.sam
+
+#===E2===
+
+#===B3===
+
+echo "samtools -- building sorted bam"
+
+mkdir -p $bamDir
+if [ ! -d "$bamDir" ]; then
+    echo "Error mkdir $bamDir"
+    exit 1
+fi
+
+samtools view -bT $genomeFa ${bwaSamDir}${sampleName}_aln.sam \
+	 > ${bamDir}${sampleName}_aln.bam
+
+samtools sort ${bamDir}${sampleName}_aln.bam \
+	 > ${bamDir}${sampleName}_aln.sorted.bam
+
+#===E3===
+
+#===B4===
+
+echo "picard -- removing duplicates"
+
+picard MarkDuplicates \
+       I=${bamDir}${sampleName}_aln.sorted.bam \
+       O=${bamDir}${sampleName}_aln.sorted_no_duplicates.bam \
+       M=${bamDir}${sampleName}_aln.sorted_no_duplicates_metrics.txt \
+       REMOVE_DUPLICATES=true
+
+samtools index -b ${bamDir}${sampleName}_aln.sorted_no_duplicates.bam
+
+#===E4===
+
+#===B5===
+
+echo "samtools mpileup -- building mpileup"
+
+samtools mpileup \
+	 -f $genomeFa ${bamDir}${sampleName}_aln.sorted_no_duplicates.bam \
+	 --output ${bamDir}${sampleName}_aln.sorted_no_duplicates.mpileup
+
+#===E5===
+
+#===B6==
+
+echo "varscan -- calling SNPs"
+
+mkdir -p $vcfDir
+if [ ! -d "$vcfDir" ]; then
+    echo "Error mkdir $vcfDir"
+    exit 1
+fi
+
+varscan pileup2snp \
+	${bamDir}${sampleName}_aln.sorted_no_duplicates.mpileup \
+	--min-var-freq 0.01 \
+	--p-value 1 \
+	> ${vcfDir}${sampleName}_aln.sorted_no_duplicates.vcf
+
+#===E6===
+
+
+### end of file variant_calling_pipeline_paired_end.sh
